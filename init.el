28dbe9a0bab3883165d65c2de759dea5b7e309a3
Dustin Neumann <dneumann@mhighpoint.com>
2021-10-10 07:55:56 -0700

    Building on my build

---
+++ init.el
@@ -0,0 +1,15 @@
+;; Bootstrap the org based config
+(org-babel-load-file "~/.config/emacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lua-mode magit evil-collection sexy-monochrome-theme evil use-package nil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
