f3ef934269fbf41782d2f37ff2f7760c5bc25bcc
Dustin Neumann <dneumann@mhighpoint.com>
2021-10-18 19:32:27 -0700

    Modified

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lua-mode magit evil-collection sexy-monochrome-theme evil use-package nil)))
+   '(blacken py-autopep8 flycheck elpy lua-mode magit evil-collection sexy-monochrome-theme evil use-package nil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
