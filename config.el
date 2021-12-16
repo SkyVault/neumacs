4a4bed10cbebcbcedfa175279ab9128079a4b8ed
dneumann <dneumann@mhighpoint.com>
2021-12-16 12:52:06 -0700

    Simple base config

---
+++ config.el
@@ -0,0 +1,61 @@
+;; Package setup
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; use-package to simplify the config file
+
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+;; User interface
+
+(setq inhibit-startup-message t
+      initial-scratch-message "Welcome Dustin!")
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Editor behaviour
+
+(setq make-backup-files nil)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;;; Theme
+
+(use-package moe-theme
+  :config
+  (require 'moe-theme)
+  (moe-dark))
+
+;; Evil mode
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-want-integration t)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (evil-collection-init))
+
+;; Magit
+
+(use-package magit
+  :ensure t
+  :config)
--- init.el
+++ init.el
@@ -1,12 +1,14 @@
-;; Bootstrap the org based config
-(org-babel-load-file "~/.config/emacs/config.org")
+(setq config-file (expand-file-name "config.el" user-emacs-directory))
+
+(when (file-exists-p config-file)
+  (load config-file))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(blacken py-autopep8 flycheck elpy lua-mode magit evil-collection sexy-monochrome-theme evil use-package nil)))
+   '(which-key magit evil-collection evil moe-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
