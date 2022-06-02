ad1c84ced50400d9ec748fbbefa709007dae79fa
dneumann <dneumann@mhighpoint.com>
2022-06-01 17:05:39 -0700

    Added config for elfeed and magit

--- package-quickstart.el
+++ package-quickstart.el
@@ -5662,6 +5662,106 @@ the mode if ARG is omitted or nil, and toggle it if ARG is `toggle'.
 
 
 )
+(let ((load-true-file-name "/home/dustin/.emacs.d/elpa/elfeed-20210822.2129/elfeed-autoloads.el")(load-file-name "/home/dustin/.emacs.d/elpa/elfeed-20210822.2129/elfeed-autoloads.el"))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory "/home/dustin/.emacs.d/elpa/elfeed-20210822.2129/elfeed-autoloads.el") (car load-path))))
+
+
+
+(autoload 'elfeed-update "elfeed" "\
+Update all the feeds in `elfeed-feeds'." t nil)
+
+(autoload 'elfeed "elfeed" "\
+Enter elfeed." t nil)
+
+(autoload 'elfeed-load-opml "elfeed" "\
+Load feeds from an OPML file into `elfeed-feeds'.
+When called interactively, the changes to `elfeed-feeds' are
+saved to your customization file.
+
+\(fn FILE)" t nil)
+
+(autoload 'elfeed-export-opml "elfeed" "\
+Export the current feed listing to OPML-formatted FILE.
+
+\(fn FILE)" t nil)
+
+(register-definition-prefixes "elfeed" '("elfeed-"))
+
+
+
+(register-definition-prefixes "elfeed-csv" '("elfeed-csv-"))
+
+
+
+(register-definition-prefixes "elfeed-curl" '("elfeed-curl-"))
+
+
+
+(register-definition-prefixes "elfeed-db" '("elfeed-" "with-elfeed-db-visit"))
+
+
+
+(register-definition-prefixes "elfeed-lib" '("elfeed-"))
+
+
+
+(autoload 'elfeed-link-store-link "elfeed-link" "\
+Store a link to an elfeed search or entry buffer.
+
+When storing a link to an entry, automatically extract all the
+entry metadata.  These can be used in the capture templates as
+%:elfeed-entry-<prop>.  See `elfeed-entry--create' for the list
+of available props." nil nil)
+
+(autoload 'elfeed-link-open "elfeed-link" "\
+Jump to an elfeed entry or search.
+
+Depending on what FILTER-OR-ID looks like, we jump to either
+search buffer or show a concrete entry.
+
+\(fn FILTER-OR-ID)" nil nil)
+
+(eval-after-load 'org `(funcall ',(lambda nil (if (version< (org-version) "9.0") (with-no-warnings (org-add-link-type "elfeed" #'elfeed-link-open) (add-hook 'org-store-link-functions #'elfeed-link-store-link)) (with-no-warnings (org-link-set-parameters "elfeed" :follow #'elfeed-link-open :store #'elfeed-link-store-link))))))
+
+
+
+(register-definition-prefixes "elfeed-log" '("elfeed-log"))
+
+
+
+(autoload 'elfeed-search-bookmark-handler "elfeed-search" "\
+Jump to an elfeed-search bookmarked location.
+
+\(fn RECORD)" nil nil)
+
+(autoload 'elfeed-search-desktop-restore "elfeed-search" "\
+Restore the state of an elfeed-search buffer on desktop restore.
+
+\(fn FILE-NAME BUFFER-NAME SEARCH-FILTER)" nil nil)
+
+(add-to-list 'desktop-buffer-mode-handlers '(elfeed-search-mode . elfeed-search-desktop-restore))
+
+(register-definition-prefixes "elfeed-search" '("elfeed-s"))
+
+
+
+(autoload 'elfeed-show-bookmark-handler "elfeed-show" "\
+Show the bookmarked entry saved in the `RECORD'.
+
+\(fn RECORD)" nil nil)
+
+(register-definition-prefixes "elfeed-show" '("elfeed-"))
+
+
+
+(register-definition-prefixes "xml-query" '("xml-query"))
+
+
+
+
+)
 (let ((load-true-file-name "/home/dustin/.emacs.d/elpa/ag-20201031.2202/ag-autoloads.el")(load-file-name "/home/dustin/.emacs.d/elpa/ag-20201031.2202/ag-autoloads.el"))
 
 (add-to-list 'load-path (directory-file-name
@@ -5777,7 +5877,7 @@ Kill all `ag-mode' buffers other than the current buffer." t nil)
 )
 (setq package-activated-list
       (append
-       '(compat with-editor vertico bind-key use-package projectile dash s avy ace-window pfuture lv hydra ht posframe cfrs treemacs treemacs-projectile persp-mode treemacs-persp transient git-commit magit-section magit treemacs-magit treemacs-icons-dired rainbow-mode parent-mode org-bullets highlight-numbers ag)
+       '(compat with-editor vertico bind-key use-package projectile dash s avy ace-window pfuture lv hydra ht posframe cfrs treemacs treemacs-projectile persp-mode treemacs-persp transient git-commit magit-section magit treemacs-magit treemacs-icons-dired rainbow-mode parent-mode org-bullets highlight-numbers elfeed ag)
        package-activated-list))
 (progn
   (require 'info)
