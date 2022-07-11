14a9404c5e6c9fa553df6f4aeb86048ae32e36a0
dneumann <dneumann@mhighpoint.com>
2022-07-11 14:29:56 -0700

    Added Which key

--- init.el
+++ init.el
@@ -7,7 +7,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(good-scroll smooth-scroll auto-complete doom-modeline beacon windmove shell-pop vertico use-package treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired rainbow-mode org-bullets highlight-numbers elfeed ag)))
+   '(which-key good-scroll smooth-scroll auto-complete doom-modeline beacon windmove shell-pop vertico use-package treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired rainbow-mode org-bullets highlight-numbers elfeed ag))
+ '(warning-suppress-types '((use-package) ((package reinitialization)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
