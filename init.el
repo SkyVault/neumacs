d8a2fa1058a24c6b7b7decae518f432332625fb1
dneumann <dneumann@mhighpoint.com>
2022-07-21 08:26:01 -0700

    Making my editor better

--- init.el
+++ init.el
@@ -7,8 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(deadgrep which-key good-scroll smooth-scroll auto-complete doom-modeline beacon windmove shell-pop vertico use-package treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired rainbow-mode org-bullets highlight-numbers elfeed ag))
- '(warning-suppress-types '((use-package) ((package reinitialization)))))
+   '(smartparens lsp-mode company corfu sly olivetti haskell-mode good-scroll smooth-scroll auto-complete doom-modeline beacon windmove shell-pop vertico use-package treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired rainbow-mode org-bullets highlight-numbers elfeed ag)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
